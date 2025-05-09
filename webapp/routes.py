ec45b169bc738d74738507740c049dc92ebd4647
Katarzyna Wojtaszczyk <sdkasik152@gmail.com>
2025-05-09 11:12:41 +0200

    continuing with flask development

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from webapp import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Player'}
+    return render_template('index.html', title='Main Menu', user=user)
